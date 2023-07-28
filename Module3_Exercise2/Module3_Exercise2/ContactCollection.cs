a038159264c3c2755c571fbda8cbf9303d90fd96
Vladyslav Varvashenko <vladyslav.varvashenko@stryker.com>
2023-07-28 09:40:21 +0300

    Add code.

---
+++ Module3_Exercise2/Module3_Exercise2/ContactCollection.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Module3_Exercise2;
+
+public sealed class Contact
+{
+    public string Name { get; set; }
+    public string PhoneNumber { get; set; }
+}
+
+public sealed class ContactCollection
+{
+    private readonly Dictionary<CultureInfo, SortedDictionary<string, List<Contact>>> _contactsByCulture;
+
+    public ContactCollection()
+    {
+        _contactsByCulture = new Dictionary<CultureInfo, SortedDictionary<string, List<Contact>>>();
+    }
+
+    public void AddContact(Contact contact, CultureInfo cultureInfo = null)
+    {
+        if (cultureInfo == null)
+        {
+            cultureInfo = CultureInfo.GetCultureInfo("en-US"); // Default culture: English
+        }
+
+        if (!_contactsByCulture.ContainsKey(cultureInfo))
+        {
+            var alphabet = GetAlphabetForCulture(cultureInfo);
+            _contactsByCulture[cultureInfo] = new SortedDictionary<string, List<Contact>>(alphabet);
+        }
+
+        string groupName = GetGroupName(contact, cultureInfo);
+
+        if (!_contactsByCulture[cultureInfo].TryGetValue(groupName, out var contacts))
+        {
+            contacts = new List<Contact>();
+            _contactsByCulture[cultureInfo][groupName] = contacts;
+        }
+
+        contacts.Add(contact);
+    }
+
+    public List<Contact> GetContacts(CultureInfo cultureInfo)
+    {
+        if (!_contactsByCulture.ContainsKey(cultureInfo))
+            return new List<Contact>();
+
+        var contacts = _contactsByCulture[cultureInfo]
+            .SelectMany(kv => kv.Value)
+            .ToList();
+
+        return contacts;
+    }
+
+    private IComparer<string> GetAlphabetForCulture(CultureInfo cultureInfo)
+    {
+        if (cultureInfo.TwoLetterISOLanguageName == "uk")
+        {
+            // Ukrainian alphabet
+            return StringComparer.Create(cultureInfo, true);
+        }
+
+        // Default: English alphabet
+        return StringComparer.InvariantCultureIgnoreCase;
+    }
+
+    private string GetGroupName(Contact contact, CultureInfo cultureInfo)
+    {
+        string alphabet = contact.Name.Substring(0, 1);
+        if (!string.IsNullOrEmpty(alphabet))
+        {
+            return char.IsDigit(alphabet[0]) ? "0-9" : alphabet.ToUpper(cultureInfo);
+        }
+
+        return char.IsDigit(alphabet[0]) ? "0-9" : alphabet.ToUpper(cultureInfo);
+    }
+}
--- Module3_Exercise2/Module3_Exercise2/Program.cs
+++ Module3_Exercise2/Module3_Exercise2/Program.cs
@@ -1,9 +1,33 @@
-﻿namespace Module3_Exercise2;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 
-internal class Program
+namespace Module3_Exercise2;
+
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        ContactCollection contacts = new ContactCollection();
+
+        Contact contact1 = new Contact { Name = "Alice 1", PhoneNumber = "123-456-7890" };
+        Contact contact11 = new Contact { Name = "Alice 2", PhoneNumber = "123-456-7899" };
+
+        Contact contact2 = new Contact { Name = "Василь 1", PhoneNumber = "987-654-3210" };
+        Contact contact22 = new Contact { Name = "Василь 2", PhoneNumber = "987-654-3210" };
+
+        contacts.AddContact(contact1, CultureInfo.GetCultureInfo("en-US"));
+        contacts.AddContact(contact11, CultureInfo.GetCultureInfo("en-US"));
+        contacts.AddContact(contact2, CultureInfo.GetCultureInfo("uk-UA"));
+        contacts.AddContact(contact22, CultureInfo.GetCultureInfo("uk-UA"));
+
+        CultureInfo selectedCulture = CultureInfo.GetCultureInfo("uk-UA");
+        List<Contact> selectedContacts = contacts.GetContacts(selectedCulture);
+
+        Console.WriteLine($"Contacts in {selectedCulture.Name}:");
+        foreach (var contact in selectedContacts)
+        {
+            Console.WriteLine($"Name: {contact.Name}, Phone: {contact.PhoneNumber}");
+        }
     }
 }
